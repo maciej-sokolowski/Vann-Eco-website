9531febca9fa42ea30204f3cd78b21f34e2d4462
maciek-sokolowski <maciek.sokolowskii@gmail.com>
2019-11-28 14:51:18 +0100

    first commit

---
+++ src/scripts/index.js
@@ -0,0 +1,11 @@
+import '../styles/index.scss';
+
+
+//hamburger animations
+
+let hamburgerAnimation = function() {
+  this.classList.toggle("change");
+  document.querySelector(".header__nav-list").classList.toggle("header__nav-list--active")
+}
+
+document.querySelector(".header__hamburger").addEventListener("click", hamburgerAnimation);
